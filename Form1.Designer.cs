f339c3a6eb87b8df1b58f94e757cbc2da4ecc21c
Vasily <Hapankoff@yandex.ru>
2020-06-23 13:06:27 +0300

    all project

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DeColors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 39);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DeColors";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeColors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TopMost = true;
+            this.MouseDown += delegate
+            {
+                this.Capture = false;
+                var msg = Message.Create(this.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
+                this.WndProc(ref msg);
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = Clipboard.GetImage();
+            Bitmap bm = new Bitmap(img);
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for(int j = 0; j < bm.Height; j++){
+                    Color old = bm.GetPixel(i, j);
+                    byte c = (byte)((old.R + old.G + old.B)/3);
+                    bm.SetPixel(i, j, Color.FromArgb(c,c,c));
+                }
+            }
+            img = (Image)bm;
+            Clipboard.SetImage(img);
+        }
+    }
+}
